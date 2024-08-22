ca9609489bb01b06e85adc0d212c20b2a5c58490
p11q <149008670+p11q@users.noreply.github.com>
2024-08-22 10:21:55 +0300

    Imported the Json library

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,11 +1,9 @@
 #include <client.h>
 
-
-
 int main() {
     const int ret = start_client();
     if (ret == 1) {
-        std::cerr << "[ERROR] The client could not be started" << std::endl;
+        std::cerr << m_time() << "[ERROR] The client could not be started" << std::endl;
     }
     return 0;
 }
--- Client/include/client.h
+++ Client/include/client.h
@@ -4,6 +4,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ctime>
+#include <nlohmann/json.hpp>
 
 inline std::string m_time(const time_t now = time(nullptr)) {
     const tm *ltm {localtime(&now)};
@@ -19,7 +20,7 @@ inline int start_client() {
     // 0 - протокол по умолчанию
 
     if (sock_fd == -1) {
-        std::cerr << "[ERROR] Socket()" << std::endl;
+        std::cerr << m_time() << "[ERROR] Socket()" << std::endl;
         return 1;
     }
 
@@ -33,23 +34,23 @@ inline int start_client() {
 
     const int ret = connect(sock_fd, reinterpret_cast<const struct sockaddr*>(&addr),sizeof(addr));
         if (ret == -1) {
-        std::cerr << "[ERROR] Connection ERROR to " << inet_ntoa(addr.sin_addr) << std::endl;
+        std::cerr << m_time() << "[ERROR] Connection ERROR to " << inet_ntoa(addr.sin_addr) << std::endl;
         return 1;
     }
 
     std::cout << m_time() << "[CLIENT] Connection to " << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << " established!" << std::endl;
 
     // Для отправки данных на сервер
-    std::string msg{"Hello! I,m Victor"};
+    const std::string msg{"Hello! I,m Victor"};
     if (const size_t transmitted = send(sock_fd, msg.data(), msg.size(), 0); transmitted != msg.size()) {
-        std::cerr << "[ERROR] not all data transmitted" << std::endl;
+        std::cerr << m_time() << "[ERROR] not all data transmitted" << std::endl;
         return 1;
     }
 
     // Для получения данных от сервера
     char buffer[64] = {'\0'};
     if (const ssize_t received = recv(sock_fd, buffer, sizeof(buffer), 0); received <= 0) {
-        puts("[ERROR] Error recv()");
+        std::cerr << m_time() << "[ERROR] Error recv()" << std::endl;
     }
     std::cout << m_time() << "[SERVER] " << buffer << std::endl;
 
