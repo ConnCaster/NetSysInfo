106bc94c93b9d6ece4946f2f918225d702e2fc2a
p11q <149008670+p11q@users.noreply.github.com>
2025-02-10 06:51:36 +0300

    cli_conection -> utils

--- sources/client/cli_connection.cpp
+++ sources/client/cli_connection.cpp
@@ -1,58 +1,9 @@
-#include <iostream>
-#include <unordered_set>
 #include <fstream>
+#include <iostream>
 
 #include "cli_connection.h"
 #include "utils.h"
 
-
-// TODO (Viktor): Добавить деструктор для unique (decltype) Прочитать про это в книжке "42 совета ..."
-
-// Поиск наименования жесткого диска в системе
-inline std::string findDeviceHardDisk () {
-
-    std::ifstream pathHardDisk ("/proc/mounts");
-    if (!pathHardDisk.is_open()) std::cerr << "[ERROR] File not open" << std::endl;
-
-    MountEntry hardDisk;
-
-    while (!pathHardDisk.eof()) {
-        pathHardDisk >> hardDisk;
-        const std::unordered_set<std::string> kFsTypesSet{"ext2", "ext3", "ext4"};
-        for (auto it : kFsTypesSet) {
-            if (hardDisk.GetFsType() == it) {
-                return hardDisk.GetDevice();
-            }
-        }
-    }
-}
-
-// Получение серийного номера жесткого диска
-inline std::string serialHardDdrive () {
-    char c_serial[20];
-
-    const std::string strPathHardDisk = "udevadm info --query=all --name=" + findDeviceHardDisk() + " | grep ID_SERIAL_SHORT";
-    std::unique_ptr<FILE, decltype(&pclose)> fp (popen(strPathHardDisk.data(), "r"), pclose);
-
-    if (fp == nullptr) std::cerr << "[ERROR] File not open" << std::endl;
-
-    while (fgets(c_serial, sizeof(c_serial), fp.get()) != nullptr) {} // Почему-то не работает, если я вместо с_serial пишу str.data() (заранее созданный)
-
-    // TODO (Viktor): нет проверки работы fgets()
-
-    std::string s_serial{std::string(c_serial)}; //22413C462705
-
-    return s_serial;
-}
-
-// Для ввода никнейма
-inline std::string nickname() {
-    std::cout << "Enter your nickname: ";
-    std::string nicknmae{};
-    std::cin >> nicknmae;
-
-    return nicknmae;
-}
 
 Conection::Conection(const int sock_fd)
     : socket_fd_(sock_fd), input_buffer_(), output_buffer_() {
@@ -64,7 +15,7 @@ void Conection::DoStart() {
     nlohmann::json im_json;
     im_json["action"] = "authUser";
 
-    im_json["args"] = { {"name", nickname()}, {"serial", serialHardDdrive()} };
+    im_json["args"] = { {"name", Nickname()}, {"serial", SerialHardDdrive()} };
     SendMsg(im_json);
 }
 
